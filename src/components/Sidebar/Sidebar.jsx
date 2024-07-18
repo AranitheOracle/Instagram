de4dc0e30c41731380f6130c402b0d02aadc92a3
Arani Ghosh <147254190+AranitheOracle@users.noreply.github.com>
2024-07-18 11:21:17 +0530

    sidebar started

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,42 +1,13 @@
-import { Avatar, Box, Flex, Link, Tooltip, Button } from "@chakra-ui/react";
+import { Box, Button, Flex, Link, Tooltip } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
-import {
-  CreatePostLogo,
-  InstagramLogo,
-  InstagramMobileLogo,
-  NotificationsLogo,
-  SearchLogo,
-} from "../../assets/constants";
-import { AiFillHome } from "react-icons/ai";
-import { BiLogOut} from "react-icons/bi";
+import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
+
+import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
+import SidebarItems from "./SidebarItems";
 
 const Sidebar = () => {
-  const sidebarItems = [
-    {
-      icon: <AiFillHome size={25} />,
-      text: "Home",
-      link: "/",
-    },
-    {
-      icon: <SearchLogo />,
-      text: "Search",
-    },
-    {
-      icon: <NotificationsLogo />,
-      text: "Notification",
-    },
-    {
-      icon: <CreatePostLogo />,
-      text: "Create",
-    },
-    {
-      icon: <Avatar size={"sm"} name="Arani" src="/profilepic.png" />,
-      text: "Profile",
-      link: "/AranitheOracle",
-    },
-  ];
-  const {handleLogout,isLoggingOut} = useLogout()
+  const { handleLogout, isLoggingOut } = useLogout();
   return (
     <Box
       height={"100vh"}
@@ -48,13 +19,13 @@ const Sidebar = () => {
       left={0}
       px={{ base: 2, md: 4 }}
     >
-      <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
+      <Flex direction={"column"} gap={10} w="full" height={"full"}>
         <Link
           to={"/"}
           as={RouterLink}
           pl={2}
           display={{ base: "none", md: "block" }}
-          cursor={"pointer"}
+          cursor="pointer"
         >
           <InstagramLogo />
         </Link>
@@ -63,47 +34,23 @@ const Sidebar = () => {
           as={RouterLink}
           p={2}
           display={{ base: "block", md: "none" }}
-          cursor={"pointer"}
           borderRadius={6}
           _hover={{
             bg: "whiteAlpha.200",
           }}
           w={10}
+          cursor="pointer"
         >
           <InstagramMobileLogo />
         </Link>
         <Flex direction={"column"} gap={5} cursor={"pointer"}>
-          {sidebarItems.map((item, index) => (
-            <Tooltip
-              hasArrow
-              label={item.text}
-              placement="right"
-              key={index}
-              ml={1}
-              openDelay={500}
-              display={{ base: "block", md: "none" }}
-            >
-              <Link
-                display={"flex"}
-                to={item.link || null}
-                as={RouterLink}
-                alignItems={"center"}
-                gap={4}
-                _hover={{ bg: "whiteAlpha.400" }}
-                borderRadius={6}
-                p={2}
-                w={{ base: 10, md: "full" }}
-                justifyContent={{ base: "center", md: "flex-start" }}
-              >
-                {item.icon}
-                <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
-              </Link>
-            </Tooltip>
-          ))}
+          <SidebarItems />
         </Flex>
+
+        {/* LOGOUT */}
         <Tooltip
           hasArrow
-          label={"LogOut"}
+          label={"Logout"}
           placement="right"
           ml={1}
           openDelay={500}
@@ -121,7 +68,14 @@ const Sidebar = () => {
             justifyContent={{ base: "center", md: "flex-start" }}
           >
             <BiLogOut size={25} />
-            <Button display={{ base: "none", md: "block" }} variant={"ghost"} _hover={{bg:"transparent"}} isLoading={isLoggingOut}>LogOut</Button>
+            <Button
+              display={{ base: "none", md: "block" }}
+              variant={"ghost"}
+              _hover={{ bg: "transparent" }}
+              isLoading={isLoggingOut}
+            >
+              Logout
+            </Button>
           </Flex>
         </Tooltip>
       </Flex>
